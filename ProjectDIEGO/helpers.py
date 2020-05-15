99d13a9b52fd9135e03351c3120bf5c7e3824fff
diegocantonSV <diego.canton@epfl.ch>
2020-05-15 18:01:58 +0200

    Add files via upload

--- ProjectDIEGO/helpers.py
+++ ProjectDIEGO/helpers.py
@@ -140,3 +140,18 @@ def Overlap(l1, r1, l2, r2):
 
 def crop_image(image, bbox):
     return image[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+
+def frames_to_video(inputpath, outputpath, fps):    
+    image_array = []
+    files = [nm for nm in os.listdir(inputpath) if '.jpg' in nm]  # make sure to only load .jpg
+    files.sort()
+    for i in range(len(files)):
+        img = cv2.imread(inputpath + files[i])
+        size =  (img.shape[1],img.shape[0])
+        img = cv2.resize(img,size)
+        image_array.append(img)
+    fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
+    out = cv2.VideoWriter(outputpath,fourcc, fps, size)
+    for i in range(len(image_array)):
+        out.write(image_array[i])
+    out.release()
