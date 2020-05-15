0bb8edb8e4784670185000cd8b8cd42fce3a63e4
diegocantonSV <diego.canton@epfl.ch>
2020-05-15 13:44:31 +0200

    Add files via upload

--- helpers.py
+++ helpers.py
@@ -3,6 +3,11 @@ import os
 import skimage.io
 import numpy as np
 
+import skimage
+from skimage.measure import label, regionprops
+from skimage.filters import threshold_minimum
+from skimage import morphology
+
 def extract_images(data_path='./data/robot_parcours_1.avi'):
     video = cv2.VideoCapture(data_path)
 
@@ -41,4 +46,28 @@ def load_data():
     images = skimage.io.concatenate_images(ic)
     
     return images, images_names
+
+def biggest_label(properties):
+    # assigns to max_lab the biggest labeled object on the image
+    max_lab = properties[0]
+    for lab in properties:
+        if lab.area > max_lab.area:
+            max_lab = lab
+    return max_lab
+
+def get_arrow_bb(img):
+    mask_red = img[:,:,2] > 90
+    test_mask = np.copy(img)
+    test_mask[mask_red] = 0
+    
+    gray = skimage.color.rgb2gray(test_mask[:,:,0])
+    
+    threshold = threshold_minimum(gray)
+    binary = gray > threshold
+    close = morphology.binary_closing(binary, skimage.morphology.selem.disk(3))
+    
+    labels = label(close)
+    biggest_lab = biggest_label(regionprops(labels))
+    
+    return biggest_lab.bbox
     
