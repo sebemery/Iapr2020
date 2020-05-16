72a04eb3b494a2fba4d42fd722c485059e5e588a
diegocantonSV <diego.canton@epfl.ch>
2020-05-16 11:11:44 +0200

    Add files via upload

--- ProjectDIEGO/helpers.py
+++ ProjectDIEGO/helpers.py
@@ -56,6 +56,22 @@ def biggest_label(properties):
             max_lab = lab
     return max_lab
 
+def get_arrow_bb_test(img):
+    mask_red = img[:,:,2] > 90
+    test_mask = np.copy(img)
+    test_mask[mask_red] = 0
+    
+    gray = skimage.color.rgb2gray(test_mask[:,:,0])
+    
+    threshold = threshold_minimum(gray)
+    binary = gray > threshold
+    close = morphology.binary_closing(binary, skimage.morphology.selem.disk(3))
+    
+    labels = label(close)
+    biggest_lab = biggest_label(regionprops(labels))
+    
+    return biggest_lab.bbox, biggest_lab.orientation, biggest_lab.minor_axis_length, biggest_lab.major_axis_length, biggest_lab.centroid
+
 def get_arrow_bb(img):
     mask_red = img[:,:,2] > 90
     test_mask = np.copy(img)
